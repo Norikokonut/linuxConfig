1adc0aa6dabed88f4fbd42d1c29377a73d30e79c
Norikokonut <125033325+Norikokonut@users.noreply.github.com>
2023-02-11 22:31:13 +0100

    Remove my login and Add commentary

--- setup.sh
+++ setup.sh
@@ -6,12 +6,11 @@ mkdir -p error
 chmod 777 *
 sudo systemctl start ssh 2> ./error/ssh.txt
 mkdir -p /media/data
+# change "noraspberry" by your own hostname
 sudo mount /dev/sda1 /media/data -o uid=noraspberry,gid=noraspberry 2> ./error/disk.txt
 
 # Git
 
-git config --global user.name "Norikoknut"
-git config --global user.email "7up.45430@gmail.com"
 git config --global credential.helper "cache --timeout 7200"
 git push 2> ./error/git.txt
 
