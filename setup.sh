7f381bc8d527da53c7af975d847c8b4a79ab53aa
Norikokonut <125033325+Norikokonut@users.noreply.github.com>
2023-02-27 15:48:53 +0100

    errors in the shell

--- setup.sh
+++ setup.sh
@@ -3,16 +3,15 @@
 #SSH + disk
 
 mkdir -p error
-chmod 777 *
-sudo systemctl start ssh 2> ./error/ssh.txt
+sudo systemctl start ssh
 mkdir -p /media/data
 # change "noraspberry" by your own hostname
-sudo mount /dev/sda1 /media/data -o uid=noraspberry,gid=noraspberry 2> ./error/disk.txt
+sudo mount /dev/sda1 /media/data -o uid=noraspberry,gid=noraspberry
 
 # Git
 
 git config --global credential.helper "cache --timeout 7200"
-git push 2> ./error/git.txt
+git push
 
 # now enter your login and your password and it's done
 
